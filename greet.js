5192255674542e7bf4e2f3bd3c417ebc6fdf3dbd
NgomsoGaya <ngomso.gaya09@gmail.com>
2023-07-17 16:11:33 +0200

    initial commit

---
+++ greet.js
@@ -0,0 +1,8 @@
+function greet (neighboursname){
+return "Hello, " + neighboursname
+};
+greet("Raadiyah")
+var greeting = greet("Raadiyah")
+const msg = greet('Ngomso')
+console.log(greeting)
+console.log(msg)
